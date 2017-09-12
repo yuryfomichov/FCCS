8cb02130cec295a9e4767dc7846c85d687610e8b
y.fomichev <yury.fomichov@gmail.com>
2017-09-12 12:58:53 +0300

    fix

--- src/prepare-data.py
+++ src/prepare-data.py
@@ -166,7 +166,7 @@ def check_accuracy(model, loader):
 
 
 def main():
-    model = get_model()
+    model = load_model()
     loss_fn = nn.CrossEntropyLoss().type(data_type)
     optimizer = optim.Adam(model.parameters(), lr=1e-2)
     train(model, loss_fn, optimizer, num_epochs=3)
