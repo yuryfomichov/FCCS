996e4be71d88848ed099a174daaa2174e785f35b
y.fomichev <yury.fomichov@gmail.com>
2017-09-13 14:05:58 +0300

    update parameters

--- src/start.py
+++ src/start.py
@@ -7,14 +7,14 @@ def run():
     network = NetworkModel(data_type=torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor,
                            model_filename="model.pt",
                            create_new=True,
-                           print_every=10,
+                           print_every=20,
                            loader_params={
-                               'batch_size': 200,
+                               'batch_size': 300,
                                'num_workers': 32 if torch.cuda.is_available() else 0
                            })
     loss_fn = nn.CrossEntropyLoss().type(network.data_type)
-    network.train(loss_fn, optim.Adam(network.model.parameters(), lr=5e-4), num_epochs=3)
-    network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-4), num_epochs=3)
-    network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-5), num_epochs=3)
+    network.train(loss_fn, optim.Adam(network.model.parameters(), lr=5e-4), num_epochs=1)
+    network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-4), num_epochs=2)
+    network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-5), num_epochs=2)
 
 run()
