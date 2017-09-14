f08b6825348c6f18091e27c9f71a1eb164b85c8a
y.fomichev <yury.fomichov@gmail.com>
2017-09-14 13:39:18 +0300

    update model

--- src/model/datasetloader.py
+++ src/model/datasetloader.py
@@ -48,8 +48,8 @@ class DatasetLoader(object):
 
     def input_transform(self):
         transform = transforms.Compose([
-            transforms.Scale(128),
-            transforms.RandomCrop(112),
+            transforms.Scale(256),
+            transforms.RandomCrop(224),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
