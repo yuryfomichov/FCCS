6bedb8984885a1ad2c3a21917642217e184e74b8
y.fomichev <yury.fomichov@gmail.com>
2017-09-15 14:22:35 +0300

    update model

--- src/model/model.py
+++ src/model/model.py
@@ -24,15 +24,15 @@ class Model(nn.Module):
             nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(256, 192, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(192),
+            nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
         #self._require_grad_false()
 
         self.classifier = nn.Sequential(
-            nn.Linear(192 * 7 * 7, 1024),
+            nn.Linear(128 * 7 * 7, 1024),
             nn.BatchNorm1d(1024),
             nn.ReLU(True),
             nn.Linear(1024, num_classes),
