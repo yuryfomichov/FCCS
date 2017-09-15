25b5e7f5a3eabbb55bb7399eec160d79599d2909
yuryfomichov <yury.fomichov@gmail.com>
2017-09-15 21:08:52 +0300

    Update start.py
    done

--- src/start.py
+++ src/start.py
@@ -10,12 +10,12 @@ def run():
                     create_new=False,
                     print_every=20,
                     loader_params={
-                        'batch_size': 200,
+                        'batch_size': 256,
                         'num_workers': 32 if torch.cuda.is_available() else 0
                     })
     loss_fn = nn.CrossEntropyLoss().type(network.data_type)
-    network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-3, weight_decay=1e-4), num_epochs=6)
-    network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-4, weight_decay=1e-5), num_epochs=7)
-    network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-5, weight_decay=1e-6), num_epochs=7)
+    network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-3, weight_decay=1e-4), num_epochs=8)
+    network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-4, weight_decay=1e-5), num_epochs=8)
+    network.train(loss_fn, optim.Adam(network.model.parameters(), lr=1e-5, weight_decay=1e-6), num_epochs=8)
 
 run()
